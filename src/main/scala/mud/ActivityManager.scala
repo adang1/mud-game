4f58ee0fe3c45ae32c6d1bbb2cf62f9852665e9f
Anh Dang <adang1@trinity.edu>
2019-12-12 16:01:37 -0600

    final version. doesn't have the BSTMap, Heap, and MutableLL. I also discovered a bug when equipping.

--- src/main/scala/mud/ActivityManager.scala
+++ src/main/scala/mud/ActivityManager.scala
@@ -7,12 +7,13 @@ import akka.actor.Props
 import java.io.PrintStream
 import java.io.BufferedReader
 import mud.ActivityManager._
-
+//import adt.BinaryHeapPQ
 
 
 class ActivityManager extends Actor {
     private var curTime = 0
     val pq = new PriorityQueue[Activity](_.time < _.time)
+    // new BinaryHeapPQ[Activity]() => make a new BinaryHeapPQ
     def receive = {
         case ScheduleActivity(delay, actor, msg) =>
         pq.enqueue(Activity(curTime + delay, actor, msg))
